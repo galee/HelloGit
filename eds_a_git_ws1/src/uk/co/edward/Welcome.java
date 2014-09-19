cd74b36f6763a927581574444e544bfd331e663d
galee <edward.gale@uk.concentrix.com>
2014-09-19 11:06:28 +0100

    Initial commit

---
+++ eds_a_git_ws1/src/uk/co/edward/Welcome.java
@@ -0,0 +1,10 @@
+package uk.co.edward;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Edward - Gitface");
+	}
+
+}
