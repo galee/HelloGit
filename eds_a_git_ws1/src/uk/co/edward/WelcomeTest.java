83531eaaaf37e85e6a1a0f70b64d9edb66dd12cd
galee <edward.gale@uk.concentrix.com>
2014-10-09 10:47:14 +0100

    New test case.

---
+++ eds_a_git_ws1/src/uk/co/edward/WelcomeTest.java
@@ -0,0 +1,15 @@
+package uk.co.edward;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WelcomeTest {
+
+	@Test
+	public void testMain() {
+		assert(true);
+//		fail("Not yet implemented"); // TODO
+	}
+
+}
